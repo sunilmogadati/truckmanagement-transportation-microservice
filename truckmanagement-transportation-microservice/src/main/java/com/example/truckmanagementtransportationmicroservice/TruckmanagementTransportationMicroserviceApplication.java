7fa02b6405065c6a4373d62237fd38d0ffd2a398
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-10-07 19:39:59 -0500

    Starter Project

---
+++ truckmanagement-transportation-microservice/src/main/java/com/example/truckmanagementtransportationmicroservice/TruckmanagementTransportationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.truckmanagementtransportationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckmanagementTransportationMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckmanagementTransportationMicroserviceApplication.class, args);
+    }
+
+}
---
+++ truckmanagement-transportation-microservice/src/test/java/com/example/truckmanagementtransportationmicroservice/TruckmanagementTransportationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.truckmanagementtransportationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckmanagementTransportationMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
