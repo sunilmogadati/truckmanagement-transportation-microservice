3ba2fea577e5fba67e36faae0385f3b2395f7dc7
ItsJustAlexC <alexcampbell1124@gmail.com>
2022-10-07 21:02:10 -0400

    adjusted organization

---
+++ truckmanagement-transportation-microservice/src/main/java/com/example/truckmanagementtransportationmicroservice/repo/Repo.java
@@ -0,0 +1,5 @@
+package com.example.truckmanagementtransportationmicroservice.repo;
+
+public class Repo {
+
+}
---
+++ truckmanagement-transportation-microservice/src/main/java/com/example/truckmanagementtransportationmicroservice/service/ServicesInterface.java
@@ -0,0 +1,5 @@
+package com.example.truckmanagementtransportationmicroservice.service;
+
+public interface ServicesInterface {
+
+}
