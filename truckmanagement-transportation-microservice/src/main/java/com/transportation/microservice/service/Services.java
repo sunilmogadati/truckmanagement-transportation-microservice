39e99cd4dd64d713dbc373aa4bfd2a30cb6e7059
ItsJustAlexC <alexcampbell1124@gmail.com>
2022-10-08 16:16:15 -0400

    updated controller class, servicesInterface class, and services class

--- truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/service/Services.java
+++ truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/service/Services.java
@@ -1,5 +1,39 @@
 package com.transportation.microservice.service;
 
-public class Services {
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.RouteMatcher.Route;
+import com.transportation.microservice.model.Truck;
+
+public class Services implements ServicesInterface {
+
+  @Override
+  public ResponseEntity<Truck> getTruckById(int id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Route> getRouteById(int id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Route> getRouteDestination(String destination) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Route> getStartPoint(String start) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Route> getRouteStatusById(int id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
--- truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/service/ServicesInterface.java
+++ truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/service/ServicesInterface.java
@@ -1,5 +1,17 @@
 package com.transportation.microservice.service;
 
-public interface ServicesInterface {
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.RouteMatcher.Route;
+import com.transportation.microservice.model.Truck;
 
+public interface ServicesInterface {
+  ResponseEntity<Truck> getTruckById(int id);
+
+  ResponseEntity<Route> getRouteById(int id);
+
+  ResponseEntity<Route> getRouteDestination(String destination);
+
+  ResponseEntity<Route> getStartPoint(String start);
+
+  ResponseEntity<Route> getRouteStatusById(int id);
 }
