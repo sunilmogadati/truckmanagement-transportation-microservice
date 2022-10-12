b94aca42d685e9950de80cacd3c7eec15df82e25
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-10-11 22:22:47 -0500

    implemented all Route Service Methods in the RousteServic Interface

--- truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/Service/RouteService/RouteService.java
+++ truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/Service/RouteService/RouteService.java
@@ -8,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import javax.management.Query;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 @Service
 public class RouteService implements RouteServiceInterface  {
