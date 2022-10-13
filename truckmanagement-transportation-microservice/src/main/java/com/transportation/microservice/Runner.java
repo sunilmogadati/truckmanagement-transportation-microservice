cca900d562c7e2564b43aa4c64cae447c99b6be3
Tedros Berihu <89820713+tedros100@users.noreply.github.com>
2022-10-13 11:26:04 -0700

    addingSwagger

--- truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/Runner.java
+++ truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/Runner.java
@@ -3,8 +3,14 @@ package com.transportation.microservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import java.io.IOException;
+
+
 @SpringBootApplication()
+//@EnableSwagger2
+
 public class Runner  implements CommandLineRunner {
 
 
