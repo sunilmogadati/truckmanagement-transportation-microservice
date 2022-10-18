cfa982a69e8e4a58518565e7fda5e24db4d443ef
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-10-18 11:49:12 -0500

    removing ds layer

--- truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/Runner.java
+++ truckmanagement-transportation-microservice/src/main/java/com/transportation/microservice/Runner.java
@@ -1,15 +1,15 @@
 package com.transportation.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+////import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 import java.io.IOException;
 @SpringBootApplication()
 @EnableSwagger2
-@EnableEurekaClient
+//@EnableEurekaClient
 public class Runner{
     public static void main(String[] args) { SpringApplication.run(Runner.class, args); }
     
